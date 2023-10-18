80f8111b54cab57f81efc8ee2ac2f64b333529c2
silabs-MarkOW <41750418+silabs-MarkOW@users.noreply.github.com>
2023-10-18 15:17:05 -0500

    lswstk: Generalized USB enumeration to use "commander adapter list" (thanks Salman!), added UDP enumeration

--- lswstk.py
+++ lswstk.py
@@ -1,7 +1,12 @@
 import os
+import socket
+import time
 
-def get_dict(command,serial) :
-    fh = os.popen('commander %s -s %s'%(command,serial))
+def get_dict(command,sn=None,ip=None) :
+    if None != sn :
+        fh = os.popen('commander %s -s %s'%(command,sn))
+    else :
+        fh = os.popen('commander %s --ip %s'%(command,ip))       
     text = fh.read()
     fh.close()
     lines = text.split('\n')
@@ -30,19 +35,57 @@ def eui64_to_address(euid64) :
             octets.append(euid64[2*i:][:2])
     return':'.join(octets)
 
-PREFIX = 'usb-Silicon_Labs_J-Link'
-files = os.listdir('/dev/serial/by-id')
+def get_local(debug=True) :
+    serialNumbers = []
+    fh = os.popen('commander adapter list','r')
+    text = fh.read()
+    fh.close()
+    lines = text.split('\n')
+    for line in lines :
+        if len(line) < 14: continue
+        if 'serialNumber: ' != line[:14] : continue
+        serialNumbers.append(line[14:])
+    return serialNumbers
+
+def get_remote(debug=True) :
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    packet  =b'Discover'
+    packet += bytes(64-len(packet))
+    s.bind(('',19020))
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    s.sendto(packet,('<broadcast>',19020))
+    s.setblocking(False)
+    start = time.time()
+    timeout = start + 1
+    ips = []
+    while time.time() < timeout :
+        try :
+            data,origin = s.recvfrom(1518)
+            if 128 != len(data) : continue
+            if b'Found' != data[:5] : continue
+            if 19020 != origin[1] : continue
+            ips.append(origin[0])
+        except BlockingIOError :
+            time.sleep(.00001)
+    return ips
+
 output = [['Serial','Boards','VCOM','Device / Debug mode','SRAM','BT address']]
-for file in files :
-    if PREFIX != file[:len(PREFIX)] : continue
-    left = file.index('_OB_')+4
-    right = file.index('-if',left)
-    serialNumber = file[left:right].lstrip('0')
+boardInfos = {}
+for serialNumber in get_local() :
+    boardInfo = get_dict('adapter probe',sn=serialNumber)
+    if dict != type(boardInfo) : continue
+    boardInfos[serialNumber] = boardInfo
+
+for ip in get_remote() :
+    boardInfo = get_dict('adapter probe',ip=ip)
+    if dict != type(boardInfo) : continue
+    serialNumber = boardInfo['J-Link Serial']
+    boardInfo['IP'] = ip
+    boardInfos[serialNumber] = boardInfo
+
+for serialNumber in boardInfos :
     lineOutput = [serialNumber]
-    boardInfo = get_dict('adapter probe',serialNumber)
-    if dict != type(boardInfo) :
-        print(boardInfo)
-        quit()
+    boardInfo = boardInfos[serialNumber]
     debugMode = boardInfo['Debug Mode']
     boards = boardInfo['Part Number']
     if list != type(boards) :
@@ -51,9 +94,16 @@ for file in files :
     for b in boards :
         tl.append(b.split()[0][3:])
     lineOutput.append(','.join(tl))
-    lineOutput.append(boardInfo['VCOM Port'])
+    ip = boardInfo.get('IP')
+    if None == ip :
+        lineOutput.append(boardInfo['VCOM Port'])
+    else :
+        lineOutput.append(boardInfo['IP'])
     if 'MCU' == debugMode or 'TARGET' == debugMode :
-        deviceInfo = get_dict('device info',serialNumber)
+        if None == ip :
+            deviceInfo = get_dict('device info',sn=serialNumber)
+        else:
+            deviceInfo = get_dict('device info',ip=ip)
         if dict != type(deviceInfo) :
             print(deviceInfo)
             quit()
